481ba1bc63d0e0c2ecca667c7a33beea86780336
Rainer Friesen <rainer.friesen@valtech.com>
2021-01-28 11:09:14 +0100

    Make Exercise component dynamic

--- components/Exercise.vue
+++ components/Exercise.vue
@@ -1,166 +1,161 @@
 <template>
-  <section class="exercise-wrapper w-full grid gap-4 grid-cols-2 mb-24">
-    <div class="flex justify-end">
-      <svg width="38" height="38" viewBox="0 0 38 38">
-        <g transform="translate(-202 -2255) translate(202 2255)">
-          <circle cx="19" cy="19" r="19" fill="#000" />
-          <path
-            fill="#F6F4F1"
-            fill-rule="nonzero"
-            d="M23.795 18.652c-1.145 0-2.93.325-4.295.957-1.365-.641-3.15-.957-4.295-.957-2.072 0-6.205 1.033-6.205 3.109v2.63h21v-2.63c0-2.076-4.133-3.109-6.205-3.109zm-3.765 4.464h-9.545v-1.24c0-.536 2.443-1.736 4.773-1.736 2.329 0 4.772 1.2 4.772 1.736v1.24zm8.485 0h-7.141v-1.24c0-.456-.19-.853-.495-1.21.838-.298 1.866-.526 2.875-.526 2.324 0 4.761 1.2 4.761 1.736v1.24zm-13.363-5.314c1.871 0 3.393-1.526 3.393-3.401 0-1.875-1.522-3.401-3.393-3.401-1.872 0-3.394 1.526-3.394 3.401 0 1.875 1.522 3.4 3.394 3.4zm0-5.314c1.05 0 1.909.86 1.909 1.913 0 1.052-.86 1.913-1.91 1.913-1.05 0-1.909-.86-1.909-1.913 0-1.052.86-1.913 1.91-1.913zm8.696 5.314c1.872 0 3.394-1.526 3.394-3.401 0-1.875-1.522-3.401-3.394-3.401-1.871 0-3.393 1.526-3.393 3.401 0 1.875 1.522 3.4 3.393 3.4zm0-5.314c1.05 0 1.91.86 1.91 1.913 0 1.052-.86 1.913-1.91 1.913-1.05 0-1.909-.86-1.909-1.913 0-1.052.86-1.913 1.91-1.913z"
-          />
-        </g>
-      </svg>
-    </div>
-    <div class="content-wrapper">
-      <div class="title-box mb-8 flex justify-between">
-        <h2 class="font-bold text-4xl max-w-2xl">
-          The dissolution of the GDR in the mirror of caricatures
-        </h2>
+  <personalize :variations="variations">
+    <template v-slot:default="{ variations }">
 
-        <ul class="flex">
-          <li>
-            <svg width="24" height="24" viewBox="0 0 24 24">
-              <g transform="translate(-1020 -367)">
-                <g>
-                  <path
-                    fill="#282828"
-                    d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7 0-.24-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92 0-1.61-1.31-2.92-2.92-2.92zM18 4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7.02c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"
-                    transform="translate(1020 367)"
-                  />
-                </g>
-              </g>
-            </svg>
-          </li>
-          <li class="ml-4">
-            <svg width="24" height="24" viewBox="0 0 24 24">
-              <g fill="none" fill-rule="evenodd">
-                <g transform="translate(-1058 -366)">
-                  <path
-                    d="M0 0L24 0 24 24 0 24z"
-                    transform="translate(1058 366)"
-                  />
-                  <path
-                    fill="#000"
-                    fill-rule="nonzero"
-                    d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2v9.67z"
-                    transform="translate(1058 366)"
-                  />
-                </g>
-              </g>
-            </svg>
-          </li>
-          <li class="ml-4">
-            <svg width="24" height="24" viewBox="0 0 24 24">
-              <g fill="none" fill-rule="evenodd">
-                <g transform="translate(-1096 -366)">
-                  <path
-                    fill="#000"
-                    d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z"
-                    transform="translate(1096 366)"
-                  />
-                </g>
-              </g>
-            </svg>
-          </li>
-          <li>
-            <nuxt-link to="#" class="cta">Ask anything</nuxt-link>
-          </li>
-        </ul>
-      </div>
-
-      <ul class="tiles mt-16 w-full grid gap-2 grid-cols-2">
-        <li class="tile mb-4">
-          <img
-            class="mb-6"
-            src="https://i.twic.pics/v1/cover=1:1/resize=478x359/placeholder:282828"
-          />
-          <p class="text-sm">
-            <strong>"40 years of GDR"</strong><br />caricature of Fritz
-            Behrendt, 1989
-          </p>
-        </li>
-        <li class="tile mb-4">
-          <img
-            class="mb-6"
-            src="https://i.twic.pics/v1/cover=1:1/resize=478x359/placeholder:282828"
-          />
-          <p class="text-sm">
-            <strong
-              >"Socialism in its course stops neither ox nor donkey."</strong
-            ><br />Caricature of Horst Haitzinger using a quote from Erich
-            Honecker, August 1989
-          </p>
-        </li>
-      </ul>
-
-      <article>
-        <h3 class="font-sans font-bold mb-2">Task</h3>
-        <p class="font-sans font-bold mb-2">
-          <strong>
-            1. The dissolution of the GDR in the mirror of caricatures
-          </strong>
-        </p>
-        <ol>
-          <li>Describe the caricatures.</li>
-          <li>
-            Explain the historical context to which the caricature refers to.
-          </li>
-          <li>
-            Interpret the caricatures and explain the intention of the
-            cartoonist.
-          </li>
-        </ol>
-      </article>
-      <hr class="mt-8 border-grey mb-6" />
-      <ul class="flex mt-4">
-        <li class="flex font-sans font-bold mr-10 -mt-2">
-          <nuxt-link to="#" class="cta cta-active">Start Session</nuxt-link>
-        </li>
-        <li class="flex font-sans font-bold mr-6">
-          <svg width="24" height="25" viewBox="0 0 24 25" class="mr-2">
-            <g fill="none" fill-rule="evenodd">
-              <g transform="translate(-498 -3093) translate(498 3092)">
-                <g>
-                  <path
-                    fill="#282828"
-                    d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 9c2.7 0 5.8 1.29 6 2v1H6v-.99c.2-.72 3.3-2.01 6-2.01m0-11C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"
-                    transform="translate(0 1.745)"
-                  />
-                </g>
-              </g>
+      <section class="exercise-wrapper w-full grid gap-4 grid-cols-2 mb-24">
+        <div class="flex justify-end">
+          <svg width="38" height="38" viewBox="0 0 38 38">
+            <g transform="translate(-202 -2255) translate(202 2255)">
+              <circle cx="19" cy="19" r="19" fill="#000" />
+              <path
+                fill="#F6F4F1"
+                fill-rule="nonzero"
+                d="M23.795 18.652c-1.145 0-2.93.325-4.295.957-1.365-.641-3.15-.957-4.295-.957-2.072 0-6.205 1.033-6.205 3.109v2.63h21v-2.63c0-2.076-4.133-3.109-6.205-3.109zm-3.765 4.464h-9.545v-1.24c0-.536 2.443-1.736 4.773-1.736 2.329 0 4.772 1.2 4.772 1.736v1.24zm8.485 0h-7.141v-1.24c0-.456-.19-.853-.495-1.21.838-.298 1.866-.526 2.875-.526 2.324 0 4.761 1.2 4.761 1.736v1.24zm-13.363-5.314c1.871 0 3.393-1.526 3.393-3.401 0-1.875-1.522-3.401-3.393-3.401-1.872 0-3.394 1.526-3.394 3.401 0 1.875 1.522 3.4 3.394 3.4zm0-5.314c1.05 0 1.909.86 1.909 1.913 0 1.052-.86 1.913-1.91 1.913-1.05 0-1.909-.86-1.909-1.913 0-1.052.86-1.913 1.91-1.913zm8.696 5.314c1.872 0 3.394-1.526 3.394-3.401 0-1.875-1.522-3.401-3.394-3.401-1.871 0-3.393 1.526-3.393 3.401 0 1.875 1.522 3.4 3.393 3.4zm0-5.314c1.05 0 1.91.86 1.91 1.913 0 1.052-.86 1.913-1.91 1.913-1.05 0-1.909-.86-1.909-1.913 0-1.052.86-1.913 1.91-1.913z"
+              />
             </g>
           </svg>
+        </div>
 
-          4 - 6 group members
-        </li>
-        <li class="flex font-sans font-bold">
-          <svg width="24" height="24" viewBox="0 0 24 24" class="mr-2">
-            <g fill="none" fill-rule="evenodd">
-              <g transform="translate(-415 -865) translate(415 863)">
-                <path d="M0 0L24 0 24 24 0 24z" transform="translate(0 2)" />
-                <path
-                  fill="#000"
-                  fill-rule="nonzero"
-                  d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"
-                  transform="translate(0 2)"
-                />
-                <path
-                  fill="#000"
-                  fill-rule="nonzero"
-                  d="M12.5 7L11 7 11 13 16.25 16.15 17 14.92 12.5 12.25z"
-                  transform="translate(0 2)"
-                />
-              </g>
-            </g>
-          </svg>
+        <div class="content-wrapper">
+          <div class="title-box mb-8 flex justify-between">
+            <h2 class="font-bold text-4xl max-w-2xl">
+              {{ variations[0].fields.title }}
+            </h2>
 
-          20 minute session
-        </li>
-      </ul>
-    </div>
-  </section>
+            <ul class="flex">
+              <li>
+                <svg width="24" height="24" viewBox="0 0 24 24">
+                  <g transform="translate(-1020 -367)">
+                    <g>
+                      <path
+                        fill="#282828"
+                        d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7 0-.24-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92 0-1.61-1.31-2.92-2.92-2.92zM18 4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7.02c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"
+                        transform="translate(1020 367)"
+                      />
+                    </g>
+                  </g>
+                </svg>
+              </li>
+              <li class="ml-4">
+                <svg width="24" height="24" viewBox="0 0 24 24">
+                  <g fill="none" fill-rule="evenodd">
+                    <g transform="translate(-1058 -366)">
+                      <path
+                        d="M0 0L24 0 24 24 0 24z"
+                        transform="translate(1058 366)"
+                      />
+                      <path
+                        fill="#000"
+                        fill-rule="nonzero"
+                        d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2v9.67z"
+                        transform="translate(1058 366)"
+                      />
+                    </g>
+                  </g>
+                </svg>
+              </li>
+              <li class="ml-4">
+                <svg width="24" height="24" viewBox="0 0 24 24">
+                  <g fill="none" fill-rule="evenodd">
+                    <g transform="translate(-1096 -366)">
+                      <path
+                        fill="#000"
+                        d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z"
+                        transform="translate(1096 366)"
+                      />
+                    </g>
+                  </g>
+                </svg>
+              </li>
+              <li>
+                <nuxt-link to="#" class="cta">Ask anything</nuxt-link>
+              </li>
+            </ul>
+          </div>
+
+          <ul class="tiles mt-16 w-full grid gap-2 grid-cols-2">
+            <li class="tile mb-4">
+              <img
+                class="mb-6"
+                :src='variations[0].fields.images[0].fields.image.fields.file.url'
+              />
+              <p class="text-sm">
+                {{ variations[0].fields.images[0].fields.subtitle  }}
+              </p>
+            </li>
+            <li class="tile mb-4">
+              <img
+                class="mb-6"
+                :src='variations[0].fields.images[1].fields.image.fields.file.url'
+              />
+              <p class="text-sm">
+                {{ variations[0].fields.images[1].fields.subtitle  }}
+              </p>
+            </li>
+          </ul>
+
+          <article>
+            {{ variations[0].fields.description }}
+          </article>
+
+          <article>
+            <template lang="md">
+              # Test MD
+              {{ variations[0].fields.task }}
+            </template>
+          </article>
+
+
+          <hr class="mt-8 border-grey mb-6" />
+          <ul class="flex mt-4">
+            <li class="flex font-sans font-bold mr-10 -mt-2">
+              <a :href='variations[0].fields.miroBoardUrl' class="cta cta-active" target="_blank">Start Session</a>
+            </li>
+            <li class="flex font-sans font-bold mr-6">
+              <svg width="24" height="25" viewBox="0 0 24 25" class="mr-2">
+                <g fill="none" fill-rule="evenodd">
+                  <g transform="translate(-498 -3093) translate(498 3092)">
+                    <g>
+                      <path
+                        fill="#282828"
+                        d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 9c2.7 0 5.8 1.29 6 2v1H6v-.99c.2-.72 3.3-2.01 6-2.01m0-11C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"
+                        transform="translate(0 1.745)"
+                      />
+                    </g>
+                  </g>
+                </g>
+              </svg>
+              {{ variations[0].fields.groupSize }}
+            </li>
+            <li class="flex font-sans font-bold">
+              <svg width="24" height="24" viewBox="0 0 24 24" class="mr-2">
+                <g fill="none" fill-rule="evenodd">
+                  <g transform="translate(-415 -865) translate(415 863)">
+                    <path d="M0 0L24 0 24 24 0 24z" transform="translate(0 2)" />
+                    <path
+                      fill="#000"
+                      fill-rule="nonzero"
+                      d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"
+                      transform="translate(0 2)"
+                    />
+                    <path
+                      fill="#000"
+                      fill-rule="nonzero"
+                      d="M12.5 7L11 7 11 13 16.25 16.15 17 14.92 12.5 12.25z"
+                      transform="translate(0 2)"
+                    />
+                  </g>
+                </g>
+              </svg>
+              {{ variations[0].fields.sessionLength }}
+            </li>
+          </ul>
+        </div>
+      </section>
+    </template>
+  </personalize>
 </template>
+  
 
 <style lang="postcss">
 .exercise-wrapper {
@@ -177,3 +172,39 @@
   list-style-position: inside;
 }
 </style>
+
+<script>
+import { Personalize } from "@uniformdev/optimize-tracker-vue";
+import { contentfulOptimizeListReader } from "@uniformdev/optimize-tracker-contentful";
+
+export default {
+  name: "ImageGrid",
+  components: {
+    Personalize,
+  },
+  props: {
+    fields: {
+      type: Object,
+      default() {
+        return {
+          unfrmOptP13nList: {},
+        };
+      },
+    },
+    sys: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  computed: {
+    variations() {
+      return contentfulOptimizeListReader(this.fields.unfrmOptP13nList);
+    },
+    mounted() {
+      this.$uniformOptimize.trackBehavior(this.fields.unfrmOptIntentTag);
+    },
+  },
+};
+</script>
