7d7289cd26d74d9a08b41dd6489cd986c44a3074
Rainer Friesen <rainer.friesen@valtech.com>
2021-01-28 10:09:02 +0100

    Remove watch on youtube button on Carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -47,22 +47,6 @@
 
           <hr class="mt-12 border-grey" />
           <ul class="flex mt-4">
-            <li class="flex font-sans font-bold mr-6 text-accent">
-              <svg width="24" height="24" viewBox="0 0 24 24" class="mr-2">
-                <g fill="none" fill-rule="evenodd">
-                  <g transform="translate(-254 -1487) translate(254 1487)">
-                    <g fill="#282828" fill-rule="nonzero">
-                      <path
-                        d="M23.76 4.31s-.234-1.646-.954-2.37C21.894.987 20.871.982 20.401.926 17.043.686 12.005.686 12.005.686h-.01s-5.038 0-8.397.241C3.13.983 2.107.987 1.194 1.94.474 2.664.24 4.31.24 4.31S0 6.243 0 8.176v1.812c0 1.933.24 3.866.24 3.866s.234 1.646.954 2.37c.913.952 2.112.922 2.646 1.022 1.92.183 8.16.24 8.16.24s5.043-.008 8.401-.25c.47-.055 1.493-.06 2.405-1.011.72-.725.954-2.371.954-2.371S24 11.92 24 9.988V8.176c0-1.933-.24-3.866-.24-3.866M9.522 12.184V5.473l6.484 3.367-6.484 3.344z"
-                        transform="translate(0 3)"
-                      />
-                    </g>
-                  </g>
-                </g>
-              </svg>
-
-              Watch on Youtube
-            </li>
             <li class="flex font-sans font-bold mr-6">
               <svg width="18" height="18" viewBox="0 0 18 18" class="mr-2 mt-1">
                 <g fill="none" fill-rule="evenodd">
